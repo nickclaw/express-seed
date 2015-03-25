27cc24c1d7900df8e9b09b85b3732bd1dd19c5c3
Nicholas Clawson <nickclaw@gmail.com>
2015-03-25 14:46:02 -0700

    Basic config/clustering

---
+++ config/config.js
@@ -0,0 +1,60 @@
+var nconf = require('nconf'),
+    path = require('path'),
+    Promise = require('bluebird'),
+    winston = require('winston');
+
+require('babel/register')({
+    extensions: ['.js', '.jsx'],
+    experimental: true
+});
+
+//
+// Sets up globals and configuration
+//
+
+var env = process.env.NODE_ENV || 'development',
+    root = path.resolve(__dirname, '..');
+
+// expose bluebird Promises
+global.Promise = Promise;
+Promise.longStackTraces();
+
+// setup environment variables
+global.C = nconf
+    .file(__dirname + '/' + env + '.json')
+    .argv()
+    .get();
+
+//
+// Logging
+//
+
+winston.cli();
+
+var transports = [];
+
+if (C.LOGGING.CONSOLE.ENABLED) {
+    transports.push(new winston.transports.Console({
+        level: C.LOGGING.CONSOLE.LEVEL,
+        colorize: true,
+        prettyPrint: true,
+        depth: 3
+    }));
+}
+
+if (C.LOGGING.FILE.ENABLED) {
+    transports.push(new winston.transports.File({
+        handleException: true,
+        timestamp: true,
+        level: C.LOGGING.CONSOLE.LEVEL,
+        colorize: false,
+        depth: 3,
+        prettyPrint: true,
+        json: false,
+        filename: path.resolve(root, C.LOGGING.FILE.NAME)
+    }));
+}
+
+global.Log = new winston.Logger({
+    transports: transports
+}).cli();
